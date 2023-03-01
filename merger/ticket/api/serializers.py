04a3bf02b22ce83e9ccebb78e843dadae176596c
AndreaBellomia <andrygam3@gmail.com>
2023-03-01 11:44:17 +0100

    "Difine serilizer, model and view for ticketapp"

--- merger/ticket/api/serializers.py
+++ merger/ticket/api/serializers.py
@@ -1,16 +1,153 @@
 from rest_framework     import serializers
-from ticket.models      import Ticket, TicketType
+from ticket.models      import (ElementRadio, ElementDropDown, ElementCeckBox,
+                                FieldInputText, FieldTextArea, FieldCeckBox, 
+                                GroupRadioButton, GroupDropDown, GroupCeckBox,
+                                TicketType, Ticket, TicketField)
 
-class TicketsTypeSerializer(serializers.ModelSerializer):
+
+
+
+
+""" Element Section """
+class ElementRadioSerializer(serializers.ModelSerializer):
+    """
+    Serilize a related_name for a FieldType 
+    """
+    class Meta:
+        model = ElementRadio
+        fields = "__all__"
+
+
+class ElementDropDownSerializer(serializers.ModelSerializer):
+    """
+    Serilize a related_name for a FieldType 
+    """
+    class Meta:
+        model = ElementDropDown
+        fields = "__all__"
+
+
+class ElementCeckBoxSerializer(serializers.ModelSerializer):
+    """
+    Serilize a related_name for a FieldType 
+    """
+    class Meta:
+        model = ElementCeckBox
+        fields = "__all__"
+
+
+
+
+
+
+
+
+""" Group Section """
+class GroupRadioButtonSerializer(serializers.ModelSerializer):
+    """
+    Serilize a related_name for a FieldType 
+    """
+
+    input_group_radiogroup = ElementRadioSerializer(many=True)
+
+    class Meta:
+        model = GroupRadioButton
+        fields = "__all__"
+
+
+class GroupDropDownSerializer(serializers.ModelSerializer):
+    """
+    Serilize a related_name for a FieldType 
+    """
+
+    input_group_dropdown = ElementDropDownSerializer(many=True)
+
+    class Meta:
+        model = GroupDropDown
+        fields = "__all__"
+
+
+class GroupCeckBoxSerializer(serializers.ModelSerializer):
+    """
+    Serilize a related_name for a FieldType 
+    """
+
+    input_group_ceckbox = ElementCeckBoxSerializer(many=True)
+
+    class Meta:
+        model = GroupCeckBox
+        fields = "__all__"
+
+
+
+
+
+
+
+
+""" Field Section """
+class FieldInputTextSerializer(serializers.ModelSerializer):
+    """
+    Serilize a related_name for a FieldType 
+    """
+    class Meta:
+        model = FieldInputText
+        fields = "__all__"
+
+class FieldTextAreaSerializer(serializers.ModelSerializer):
+    """
+    Serilize a related_name for a FieldType 
+    """
+    class Meta:
+        model = FieldTextArea
+        fields = "__all__"
+
+class FieldCeckBoxSerializer(serializers.ModelSerializer):
+    """
+    Serilize a related_name for a FieldType 
+    """
+    class Meta:
+        model = FieldCeckBox
+        fields = "__all__"
+
+
+
+
+
+
+
+class TicketTypeSerializer(serializers.ModelSerializer):
+    """  
+    TicketType Serializer without related fields
+    """
 
     class Meta:
         model = TicketType
         fields = "__all__"
+
+
+class TicketTypeRelatedSerializer(serializers.ModelSerializer):
+    """  
+    TicketType Serializer with related fields
+    """
+
+    class Meta:
+        model = TicketType
+        exclude = ('img_url', 'status')
+
+    ticket_type_radiogroup = GroupRadioButtonSerializer(many=True)
+    ticket_type_dropdown = GroupDropDownSerializer(many=True)
+    ticket_type_ceckbox = GroupCeckBoxSerializer(many=True)
+
+    ticket_type_inputtext = FieldInputTextSerializer(many=True)
+    ticket_type_textarea = FieldTextAreaSerializer(many=True)
+    ticket_type_field_ceckbox = FieldCeckBoxSerializer(many=True)
+
+
 class TicketsSerializer(serializers.ModelSerializer):
 
-    type_document = serializers.PrimaryKeyRelatedField(read_only=True)
-    type_document = TicketsTypeSerializer(type_document)
+    type_document = TicketTypeRelatedSerializer(many=True)
+    
     class Meta:
         model = Ticket
         fields = "__all__"
-
--- merger/ticket/urls.py
+++ merger/ticket/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
-
-from ticket.views import TicketApiView, TicketTipeApiView
-
+from ticket.views import TesetView, TicketTypeDatailApiView, TicketTypeView, TicketApiView
 
 
 urlpatterns = [
+    path('ticket', TesetView.as_view(), name='ticket'),
+
+    path('client/ticket-type/<int:pk>', 
+         TicketTypeDatailApiView.as_view(), 
+         name='ticket_type_datail'),
+    
+    path('client/ticket-type/', 
+         TicketTypeView.as_view(), 
+         name='ticket_type_datail'),
+
     path('client/tickets/', 
         TicketApiView.as_view(), 
-        name='ticket-list'),
+        name='ticket_list'),
 
-    path('client/ticket-tipe/', 
-        TicketTipeApiView.as_view(), 
-        name='ticket-type-list'),
+
 ]
