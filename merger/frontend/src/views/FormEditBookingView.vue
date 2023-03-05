cdad46cbe0b42f445fbfffd7ef1522f7ccf9ba9d
AndreaBellomia <andrygam3@gmail.com>
2023-03-05 19:39:30 +0100

    "form autoad frontend implement"

--- merger/frontend/src/views/FormEditBookingView.vue
+++ merger/frontend/src/views/FormEditBookingView.vue
@@ -1,51 +1,53 @@
 <template>
-    <div v-if="this.$route.params.type == 'edit-green'">
-        <HeaderForm :booking="booking" :state="'Confermato'" class="mb-8"></HeaderForm>
-        <EditBookingContentForm :booking="booking" :formTitle="'Visualizza'">
-        </EditBookingContentForm>
-        <div class="custom-fb-container left">
-            <FloatingActionButton :routerLink="'/MyBookingView'" :icon="'undo'"></FloatingActionButton>
+    <div>
+        <div v-if="this.$route.params.type == 'edit-green'">
+            <HeaderForm :booking="booking" :state="'Confermato'" class="mb-8"></HeaderForm>
+            <EditBookingContentForm :booking="booking" :formTitle="'Visualizza'">
+            </EditBookingContentForm>
+            <div class="custom-fb-container left">
+                <FloatingActionButton :routerLink="'/MyBookingView'" :icon="'undo'"></FloatingActionButton>
+            </div>
+            <div class="custom-fb-container right">
+                <FloatingActionButton :icon="'delete'" :bgColor="'#808080'" :color="'#B9B9B9'" :disabled="true">
+                </FloatingActionButton>
+            </div>
         </div>
-        <div class="custom-fb-container right">
-            <FloatingActionButton :icon="'delete'" :bgColor="'#808080'" :color="'#B9B9B9'" :disabled="true">
-            </FloatingActionButton>
-        </div>
-    </div>
 
-    <div v-if="this.$route.params.type == 'edit-orange'">
-        <PopUp :show="showPopUp" :icon="'edit'" :title="'Modificato!'" :buttonText="'Chiudi'"
-            :content="`Modifica per ${this.formatData()} avvenuta con successo! ID: ${this.booking.id}`"
-            :routerLink="'/MyBookingView'" @close-modal="showPopUp = false">
-        </PopUp>
-        <PopUpWithConfirmAction :show="showPopUpDelete" :icon="'delete'" :titleFirst="'Elimina'" :titleSecond="'Eliminato!'"
-            :contentFirst="`Vuoi eliminare l'appuntamento per ${this.formatData()}? ID: ${this.booking.id}`"
-            :contentSecond="`Appuntamento per ${this.formatData()} eliminato con successo! ID: ${this.booking.id}`"
-            :buttonTextFirst="'Conferma'" :buttonTextSecond="'Chiudi'" :routerLink="'/MyBookingView'"
-            @close-modal="showPopUpDelete = false" @action="deleteHTTP_bookingById()">
-        </PopUpWithConfirmAction>
-        <HeaderForm :booking="booking" :state="'In Attesa'" class="mb-8"></HeaderForm>
-        <EditBookingContentForm :booking="booking" :formTitle="'Modifica'" :isToSend="true" :formType="'update'"
-            @open-modal="showPopUp = true">
-        </EditBookingContentForm>
-        <div class="custom-fb-container left">
-            <FloatingActionButton :routerLink="'/MyBookingView'" :icon="'undo'"></FloatingActionButton>
+        <div v-if="this.$route.params.type == 'edit-orange'">
+            <PopUp :show="showPopUp" :icon="'edit'" :title="'Modificato!'" :buttonText="'Chiudi'"
+                :content="`Modifica per ${this.formatData()} avvenuta con successo! ID: ${this.booking.id}`"
+                :routerLink="'/MyBookingView'" @close-modal="showPopUp = false">
+            </PopUp>
+            <PopUpWithConfirmAction :show="showPopUpDelete" :icon="'delete'" :titleFirst="'Elimina'" :titleSecond="'Eliminato!'"
+                :contentFirst="`Vuoi eliminare l'appuntamento per ${this.formatData()}? ID: ${this.booking.id}`"
+                :contentSecond="`Appuntamento per ${this.formatData()} eliminato con successo! ID: ${this.booking.id}`"
+                :buttonTextFirst="'Conferma'" :buttonTextSecond="'Chiudi'" :routerLink="'/MyBookingView'"
+                @close-modal="showPopUpDelete = false" @action="deleteHTTP_bookingById()">
+            </PopUpWithConfirmAction>
+            <HeaderForm :booking="booking" :state="'In Attesa'" class="mb-8"></HeaderForm>
+            <EditBookingContentForm :booking="booking" :formTitle="'Modifica'" :isToSend="true" :formType="'update'"
+                @open-modal="showPopUp = true">
+            </EditBookingContentForm>
+            <div class="custom-fb-container left">
+                <FloatingActionButton :routerLink="'/MyBookingView'" :icon="'undo'"></FloatingActionButton>
+            </div>
+            <div class="custom-fb-container right">
+                <FloatingActionButton :icon="'delete'" :bgColor="'#FF1F00'" @click="showPopUpDelete = !showPopUpDelete">
+                </FloatingActionButton>
+            </div>
         </div>
-        <div class="custom-fb-container right">
-            <FloatingActionButton :icon="'delete'" :bgColor="'#FF1F00'" @click="showPopUpDelete = !showPopUpDelete">
-            </FloatingActionButton>
-        </div>
-    </div>
 
-    <div v-if="this.$route.params.type == 'edit-red'">
-        <HeaderForm :booking="booking" :state="'Annullato'" class="mb-8"></HeaderForm>
-        <EditBookingContentForm :booking="booking" :formTitle="'Visulizza'">
-        </EditBookingContentForm>
-        <div class="custom-fb-container left">
-            <FloatingActionButton :routerLink="'/MyBookingView'" :icon="'undo'"></FloatingActionButton>
-        </div>
-        <div class="custom-fb-container right">
-            <FloatingActionButton :icon="'delete'" :bgColor="'#808080'" :color="'#B9B9B9'" :disabled="true">
-            </FloatingActionButton>
+        <div v-if="this.$route.params.type == 'edit-red'">
+            <HeaderForm :booking="booking" :state="'Annullato'" class="mb-8"></HeaderForm>
+            <EditBookingContentForm :booking="booking" :formTitle="'Visulizza'">
+            </EditBookingContentForm>
+            <div class="custom-fb-container left">
+                <FloatingActionButton :routerLink="'/MyBookingView'" :icon="'undo'"></FloatingActionButton>
+            </div>
+            <div class="custom-fb-container right">
+                <FloatingActionButton :icon="'delete'" :bgColor="'#808080'" :color="'#B9B9B9'" :disabled="true">
+                </FloatingActionButton>
+            </div>
         </div>
     </div>
 </template>
@@ -118,6 +120,6 @@ export default {
 
 .custom-fb-container.right {
     right: 1.5rem;
-
+    padding: .5rem .5rem;
 }
 </style>
